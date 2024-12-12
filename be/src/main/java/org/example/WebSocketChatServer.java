ee93f6e370d3e0cb870931a71861e0a63cbd16a0
johan1103 <johan1103@naver.com>
2024-12-12 20:53:06 +0900

    [feature-2] reactor netty 사용으로 구현 변경

--- be/src/main/java/org/example/WebSocketChatServer.java
+++ be/src/main/java/org/example/WebSocketChatServer.java
@@ -2,101 +2,64 @@ package org.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.*;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpServerCodec;
-import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
-import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import reactor.core.publisher.Flux;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.http.websocket.WebsocketInbound;
+import reactor.netty.http.websocket.WebsocketOutbound;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WebSocketChatServer {
 
-    private static final ConcurrentHashMap<ChannelId, Channel> clients = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, WebsocketOutbound> clients = new ConcurrentHashMap<>();
     private static final Logger log = LoggerFactory.getLogger(WebSocketChatServer.class);
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static void main(String[] args) throws InterruptedException {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        DisposableServer server = HttpServer.create().port(9000)
+                .route(routes -> routes.ws("/chat", (in, out)->handleWebSocket(in, out))).bindNow();
 
+        System.out.println("WebSocket Chat Server is running on ws://localhost:9000/chat");
+        server.onDispose().block();
+    }
+
+    private static Flux<Void> handleWebSocket(WebsocketInbound inbound, WebsocketOutbound outbound) {
+        String clientId = String.valueOf(inbound.hashCode());
+        clients.put(clientId, outbound);
+
+        broadcastMessage(MessagePrefix.STRING.getType()+clientId+" joined the chat", clientId).subscribe();
+        log.info("{} joined the chat", clientId);
+
+        // 연결 종료 시 특정 로직 실행
+        inbound.receiveCloseStatus()
+                .flatMap(status ->
+                        broadcastMessage(MessagePrefix.STRING.getType()+clientId+"left the chat", clientId)
+                                .then())
+                .subscribe(); // 스트림을 구독하여 종료 상태 감지
+
+        return inbound.receive().asString()
+                .flatMap(message -> processMessage(clientId, message))
+                .flatMap(signalType -> broadcastMessage(MessagePrefix.STRING.getType()+clientId + " has left the chat", clientId));
+    }
+
+    private static Flux<Void> processMessage(String senderId, String jsonMessage) {
         try {
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup, workerGroup)
-                     .channel(NioServerSocketChannel.class)
-                     .childHandler(new ChannelInitializer<SocketChannel>() {
-                         @Override
-                         protected void initChannel(SocketChannel ch) throws Exception {
-                             ChannelPipeline pipeline = ch.pipeline();
-                             pipeline.addLast(new HttpServerCodec());
-                             pipeline.addLast(new HttpObjectAggregator(65536));
-                             pipeline.addLast(new WebSocketServerProtocolHandler("/chat"));
-                             pipeline.addLast(new WebSocketChatHandler());
-                         }
-                     });
-
-            ChannelFuture future = bootstrap.bind(8080).sync();
-            System.out.println("WebSocket Chat Server is running on ws://localhost:8080/chat");
-            future.channel().closeFuture().sync();
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
+            ClientMessage clientMessage = objectMapper.readValue(jsonMessage, ClientMessage.class);
+            ServerMessage serverMessage = ServerMessage.of(senderId, clientMessage);
+            String serverMessageStr = MessagePrefix.SERVER_MESSAGE.getType() + objectMapper.writeValueAsString(serverMessage);
+            return broadcastMessage(serverMessageStr, senderId);
+        }catch (JsonProcessingException e) {
+            log.error("Failed to process message: {}", e.getMessage());
+            return Flux.empty();
         }
     }
 
-    private static class WebSocketChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
-        @Override
-        public void channelActive(ChannelHandlerContext ctx) throws Exception {
-            String userId = ctx.channel().id().toString();
-            clients.put(ctx.channel().id(), ctx.channel());
-            broadcastMessage(userId + "A new client has joined!", userId);
-        }
-
-        @Override
-        protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
-            String jsonMessage = msg.text();
-            try {
-                String userId = ctx.channel().id().toString();
-                ClientMessage clientMessage = objectMapper.readValue(jsonMessage, ClientMessage.class);
-                ServerMessage serverMessage = ServerMessage.of(userId, clientMessage);
-                broadcastServerMessage(serverMessage, userId);
-            } catch (Exception e) {
-                e.printStackTrace();
-                log.error(e.getLocalizedMessage());
-            }
-        }
-
-        @Override
-        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-            String userId = ctx.channel().id().toString();
-            clients.remove(ctx.channel().id());
-            broadcastMessage(userId + " client has left.", userId);
-        }
-
-        private void broadcastServerMessage(ServerMessage serverMessage, String senderChannelId) throws JsonProcessingException {
-            String message = MessagePrefix.SERVER_MESSAGE.getType() + objectMapper.writeValueAsString(serverMessage);
-            log.info("jsonStr : {}", message);
-            broadcast(message, senderChannelId);
-        }
-
-        private void broadcastMessage(String message, String senderChannelId) {
-            message = MessagePrefix.STRING.getType() + message;
-            log.info("str : {}", message);
-            broadcast(message, senderChannelId);
-        }
-
-        private void broadcast(String message, String senderChannelId) {
-            for (Channel channel : clients.values()) {
-                if(channel.id().toString().equals(senderChannelId))
-                    continue;
-                channel.writeAndFlush(new TextWebSocketFrame(message));
-            }
-        }
+    private static Flux<Void> broadcastMessage(String message, String senderId) {
+        return Flux.fromIterable(clients.entrySet())
+                .filter(entry -> !entry.getKey().equals(senderId))
+                .flatMap(entry -> entry.getValue().sendString(Flux.just(message)).then());
     }
+
 }
